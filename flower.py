b3ff3fac5646f6d6bb9fad3c75ceaafa0f398447
oilzhang <18810061301@163.com>
2019-05-09 11:06:19 +0800

    完善代码

--- flower.py
+++ flower.py
@@ -47,5 +47,71 @@ def Fibonacci_sequence(n):
 	"""
 	斐波那契数列---从第三项开始，每一项都等于前两项之和
 	"""
-	
+	result = [1, 1]
+	i, j = 0, 0
+	while i < n:
+		i = result[j] + result[j+1]
+		if i < n:
+			result.append(i)
+			j += 1
+
+	return result
+
+def crap():
+	"""
+	Craps赌博游戏
+	玩家摇两颗色子 如果第一次摇出7点或11点 玩家胜
+	如果摇出2点 3点 12点 庄家胜 其他情况游戏继续
+	玩家再次要色子 如果摇出7点 庄家胜
+	如果摇出第一次摇的点数 玩家胜
+	否则游戏继续 玩家继续摇色子
+	玩家进入游戏时有1000元的赌注 全部输光游戏结束
+	"""
+	from random import randint
+
+	money = 1000
+	while money > 0:
+		print('你的总资产为：',money)
+		
+		need_go_on = False #设置一个开关---判断是否需要继续
+		
+		while True:
+			debt = int(input('请下注：'))
+			if debt > 0 and debt <= money:
+				break
+		
+		first = randint(1,6) + randint(1,6)
+		print('玩家第一次摇出%d点'%(first))
+
+		if first == 7 or first == 11:
+			print('玩家胜')
+			money += debt
+
+		elif first == 2 or first == 3 or first == 12:
+			print('庄家胜')
+			money -= debt
+		
+		else:
+			need_go_on = True
+		
+		while need_go_on:  #当玩家和庄家都没赢时，游戏继续
+			
+			current = randint(1,6) + randint(1,6)
+			print('玩家摇出%d点'%(current))
+			
+			if current == first:
+				print('玩家胜')
+				money += debt
+				need_go_on = False  #玩家胜，退出当前循环
+
+			elif current == 7:
+				print('庄家胜')
+				money -= debt
+				need_go_on = False # 庄家胜，退出当前循环
+			else:
+				need_go_on = True # 玩家和庄家都没赢时，游戏继续
+
+	print('你破产了')
+			
+
 
